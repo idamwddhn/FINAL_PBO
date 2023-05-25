2f5df107ccbdfaaefe1c41df7a7b98c2858eea96
idamwddhn <127532576+idamwddhn@users.noreply.github.com>
2023-05-25 17:15:00 +0800

    Create Aplikasi_Restoran_Sederhana.java

---
+++ Aplikasi_Final_B1_13020210026/Aplikasi_Restoran_Sederhana.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aplikasi_restoran_sederhana;
+
+/**
+ *
+ * @author LENOVO-PC
+ */
+public class Aplikasi_Restoran_Sederhana {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Login l = new Login();
+        l.setVisible(true);
+       
+    }
+    
+}
